4999353d9f3b5657c79a78ef47c40a14846f2586
DESKTOP-9STFKGG\keyma <keymasterviriya1150@gmail.com>
2024-09-17 15:41:20 +0700

    "Update Astro config and Layout component with new site URL and base path"

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://seedbkkchurch.github.io',
+  base: 'qr-code-seed',
   integrations: [react()]
 });
